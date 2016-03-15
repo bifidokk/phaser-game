ce6bdef2f3a66fe4a9918bcec0e49a67204ae33f
bifidokk <bifidokk@gmail.com>
2016-03-15 21:57:31 +0500

    map to file

---
+++ js/map.js
@@ -0,0 +1,39 @@
+/**
+ * Created by danilka on 15.03.2016.
+ */
+var MAP_COLLISION_TILES_NAME = 'collision';
+
+Map = function(game) {
+    this.game = game;
+    this.tilemap = 'level';
+    this.tileimage = 'tiles';
+    this.layers = {};
+    this.map = null;
+};
+
+Map.prototype = Object.create(Phaser.Sprite.prototype);
+Map.prototype.constructor = Map;
+
+Map.prototype.addMap = function() {
+    this.map = this.game.add.tilemap(this.tilemap);
+    this.map.addTilesetImage('blocks', this.tileimage);
+
+    this.map.layers.forEach(function (layer) {
+        this.layers[layer.name] = this.map.createLayer(layer.name);
+        if (layer.properties.collision) { // collision layer
+            var collision_tiles = [];
+            layer.data.forEach(function (data_row) {
+                data_row.forEach(function (tile) {
+                    if (tile != null && tile.index > 0 && collision_tiles.indexOf(tile.index) === -1) {
+                        collision_tiles.push(tile.index);
+                    }
+                }, this);
+            }, this);
+            this.map.setCollision(collision_tiles, true, layer.name);
+        }
+
+        this.layers[layer.name].resizeWorld();
+    }, this);
+};
+
+
